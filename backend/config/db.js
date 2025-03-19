d08f6b80c4514270c13107965b02eed6950ea36b
DELL <paulphilippecoly@gmail.com>
2025-03-19 14:49:38 +0000

    ajout

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,10 +2,13 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://localhost:27017/etudiants", {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(
+      "mongodb://mongodb+srv://paulphilippe:1234567890@render.gaco9.mongodb.net/?retryWrites=true&w=majority",
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }
+    );
     console.log("MongoDB connecté");
   } catch (error) {
     console.error("Erreur de connexion à MongoDB", error);
